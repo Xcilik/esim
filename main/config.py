f72f8f9bcab275479e0b9a026dc94638bb69b5ec
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-26 12:44:38 +0700

    Create config.py

---
+++ main/config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "7538304"))
+API_HASH = os.getenv("API_HASH", "474a43a1580ece1f0fd348040e13233b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7144522875:AAEFGYd8mx8p3wvfpBozxW5Ea5L-XEG2y2M")
+OWNER_ID = int(os.getenv("OWNER_ID", "1784606556"))
