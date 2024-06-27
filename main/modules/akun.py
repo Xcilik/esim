3261ee647adb3c9a962fa9ed0eeaf94d52a1a1e5
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-06-27 22:06:22 +0700

    Update akun.py

--- main/modules/akun.py
+++ main/modules/akun.py
@@ -1,4 +1,17 @@
 from pyrogram import filters
 from main import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+@app.on_message(filters.command("setting") & filters.private)
+async def setting(client, message):
+    text = f"""
+"""    
+    button = [
+        [
+            InlineKeyboardButton("Tambah Akun", callback_data="add_akun"),
+        ],
+        [
+           InlineKeyboardButton("Setting", callback_data="setting"),
+        ],
+    ]
