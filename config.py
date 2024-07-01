7c8831961f640ac904e068e5d50302eaf7a69e32
hidagans <58388773+hidagans@users.noreply.github.com>
2024-07-02 01:33:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 API_ID = int(os.getenv("API_ID", "7538304"))
 API_HASH = os.getenv("API_HASH", "474a43a1580ece1f0fd348040e13233b")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7144522875:AAEFGYd8mx8p3wvfpBozxW5Ea5L-XEG2y2M")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7186860110:AAH1joH2Xd8aCeKERzWeG4M40TMq5IT5dIg")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://akun:akun@cluster0.sumcfuo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.getenv("OWNER_ID", "1784606556"))
